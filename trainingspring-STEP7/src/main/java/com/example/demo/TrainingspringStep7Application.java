d1f93fcaa969fca5b5ea6cc36bdc9b47b4483c31
hiran <hiran@192.168.10.173>
2025-04-15 13:19:29 +0900

    STEP7を作成しました。

---
+++ trainingspring-STEP7/src/main/java/com/example/demo/TrainingspringStep7Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingspringStep7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingspringStep7Application.class, args);
+	}
+
+}
